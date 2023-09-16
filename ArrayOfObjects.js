61fc6aba923a269a45918cf0ba87f4ef825a0c94
surendra-72 <141621460+surendra-72@users.noreply.github.com>
2023-09-16 12:15:47 +0530

    Add files via upload

---
+++ ArrayOfObjects.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Counter from "./Counter";
+
+class ArrayOfObjects extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            CountriesInfo: [
+                {name:'India',Capital:"NewDelhi"},
+                {name:'India',Capital:"NewDelhi"},
+                {name:'India',Capital:"NewDelhi"},
+                {name:'India',Capital:"NewDelhi"},
+            ]
+        }
+    }
+
+    render () {
+        const {CountriesInfo}=this.state;
+        return (
+            <div>
+                <table border='1'>
+                    <tr>
+                        <th>Country Name</th>
+                        <th>Capital</th>
+                    </tr>
+                    {CountriesInfo.length>0&&CountriesInfo.map((Counter)=>{
+                        return <tr key={Country.name}>
+                            <td>{Country.name}</td>
+                            <td>{Country.Capital}</td>
+                        </tr>
+                    })}
+                </table>
+            </div>
+        )
+    }
+}
+
+export default ArrayOfObjects
